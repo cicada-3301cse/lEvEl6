6d370704b39fc7981929857dd809ada969b60c90
cicada-3301cse <rvscsecicada3301@gmail.com>
2024-10-24 15:57:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-function check() {
-    //alert("checked")
+// function check() {
+//     alert("checked")
+// console.log("hello")
+// }
+
+
 var pass = "259";
-console.log("hello")
+
 
 function activate(){
     var paswd = document.getElementById("ans").value;
     if (paswd == pass) {
-        console.log("hello");
+        // console.log("hello");
         //alert("correct password");
         //window.location.replace('https://www.google.com')
         window.location.href = "https://cicada-3301cse.github.io/LeVeL7/";
     }
     else{
-        console.log("hello")
         alert("wrogn password");
         
     }
 }
-}
+
